1b4a9371e384a224b47f6c67208ac751bc1c6012
pratik-jadhav23 <pratik.jadhav@mmit.edu.in>
2023-02-17 20:06:46 +0530

    done

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,15 +4,17 @@ import Date1 from "./Date"
 import Login from './Login';
 import MCQ from './MCQ';
 import TodoList from './TodoList';
+import { useState } from 'react';
 
 function App() {
   console.log("hello")
+  let [todolist,setTodolist]=useState()
   return (
     <div>
      {/* <Date1 /> */}
      {/* <Login/> */}
-     {/* <MCQ/> */}
-     <TodoList/>
+     <MCQ/>
+     {/* <TodoList/> */}
     </div>
   );
 }
