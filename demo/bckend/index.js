8d5864c34d7194c0f2be0cd45741b682e890cae7
pratik-jadhav23 <pratik.jadhav@mmit.edu.in>
2024-01-11 15:16:45 +0530

    11-01-24

--- demo/bckend/index.js
+++ demo/bckend/index.js
@@ -6,18 +6,18 @@ function fun(){
     let data=fs.readFileSync ('abc.txt','utf-8')
     return data
 }
-function async(){
-    var http
-    return ()
-}
+// function async(){
+//     var http
+//     return ()
+// }
 
 
 
 let server=http.createServer((req,res)=>{                                                                                                                                                                                                                                                                                                                                                                     
     console.log(req.url)
-    let ans=fun()+' extra text'
+    let ans=fun()+' extra text here to type'
     res.end(ans)
 })
-server.listen(5000,()=>{
+server.listen(5001,()=>{
     console.log('server started')
 })
---
+++ demo/bckend/os.js
@@ -0,0 +1,3 @@
+const os = require('os')
+console.log(os.freemem(), os.homedir(), os.hostname(), os.platform(), os.release())
+
